0ea6e2a2ce426ace7899ad0cd49261c208104227
NerdsForGaming <74607784+NerdsForGaming@users.noreply.github.com>
2023-12-24 23:20:10 +0530

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,17 +1,20 @@
-// api/ping.js
-
 const ping = require('ping');
 
 module.exports = async (req, res) => {
-  const { ip } = req.query; // Extract IP address from the query parameter
-  if (!ip) {
-    return res.status(400).json({ error: 'IP parameter is missing.' });
-  }
-
   try {
+    const { ip } = req.query; // Extract IP address from the query parameter
+    if (!ip) {
+      return res.status(400).json({ error: 'IP parameter is missing.' });
+    }
+
     const result = await ping.promise.probe(ip);
-    return res.status(200).json({ ping: result });
+    if (result.alive !== undefined && result.time !== undefined) {
+      return res.status(200).json({ ping: result.time });
+    } else {
+      return res.status(500).json({ error: 'Invalid ping response.' });
+    }
   } catch (error) {
+    console.error('Error pinging the IP address:', error);
     return res.status(500).json({ error: 'Error pinging the IP address.' });
   }
 };
