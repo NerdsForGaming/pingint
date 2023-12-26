c695070bcfd05b0977b425ce808a561fff56ca8f
NerdsForGaming <74607784+NerdsForGaming@users.noreply.github.com>
2023-12-26 23:51:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,47 +1,33 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Server Ping Test</title>
+</head>
+<body>
+    <h1>Server Ping Test</h1>
+    <form method="get">
+        <label for="ip">Enter IP address to ping:</label>
+        <input type="text" id="ip" name="ip" required>
+        <button type="submit">Ping</button>
+    </form>
 
-use Slim\Factory\AppFactory;
+    <?php
+    if (isset($_GET['ip'])) {
+        $ip = $_GET['ip'];
 
-require __DIR__ . '/vendor/autoload.php';
+        // Perform ping using system command
+        $pingResult = exec("ping -c 4 $ip"); // Sends 4 ICMP echo requests
 
-$app = AppFactory::create();
-
-$app->get('/ping', function ($request, $response, $args) {
-    $ip = $request->getQueryParam('ip');
-    if (!$ip || !filter_var($ip, FILTER_VALIDATE_IP)) {
-        $data = [
-            'error' => 'Invalid IP address provided'
-        ];
-        return $response->withJson($data, 400); // Bad Request
-    }
-
-    $pingResult = getPing($ip);
-
-    $data = [
-        'ip_address' => $ip,
-        'ping_result' => $pingResult,
-    ];
-
-    return $response->withJson($data);
-});
-
-function getPing($ip) {
-    $output = [];
-    $command = 'ping -c 4 ' . $ip;
-
-    exec($command, $output);
-
-    $ping_time = '';
-    foreach ($output as $line) {
-        if (strpos($line, 'time=') !== false) {
-            $start_pos = strpos($line, 'time=') + strlen('time=');
-            $end_pos = strpos($line, 'ms', $start_pos);
-            $ping_time = substr($line, $start_pos, $end_pos - $start_pos);
-            break;
+        // Extracting average time from ping result
+        preg_match('/time=([\d.]+) ms/', $pingResult, $matches);
+        
+        if (isset($matches[1])) {
+            $responseTime = $matches[1];
+            echo "<p>Average Ping to $ip: $responseTime ms</p>";
+        } else {
+            echo "<p>Failed to ping $ip.</p>";
         }
     }
-
-    return $ping_time !== '' ? $ping_time . ' ms' : 'Ping failed';
-}
-
-$app->run();
+    ?>
+</body>
+</html>
